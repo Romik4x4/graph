5cd66610a31a6b1f92ce57336fe5a37d9534683d
Romik <romik@debian74-64.pajero4x4.ru>
2015-02-11 14:38:07 +0300

    eeee

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@
 #include <QMetaEnum>
 #include "qcustomplot.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow)
@@ -36,28 +37,9 @@ void MainWindow::setupDemo(int demoIndex)
 
 void MainWindow::setupQuadraticDemo(QCustomPlot *customPlot)
 {
-  demoName = "Quadratic Demo";
-  // generate some data:
 
-  QVector<double> x(101), y(101); // initialize with entries 0..100
 
-  for (int i=0; i<101; ++i)
-  {
-    x[i] = i; // x goes from -1 to 1
-    y[i] = i;  // let's plot a quadratic function
-  }
-  // create graph and assign data to it:
   customPlot->addGraph();
-  customPlot->graph(0)->setData(x, y);
-  // give the axes some labels:
-  customPlot->xAxis->setLabel("x");
-  customPlot->yAxis->setLabel("y");
-  // set axes ranges, so we see all data:
-  customPlot->xAxis->setRange(-1, 1);
-  customPlot->yAxis->setRange(0, 1);
-
-
-  // setup a timer that repeatedly calls MainWindow::realtimeDataSlot:
 
   connect(&dataTimer, SIGNAL(timeout()), this, SLOT(realtimeDataSlot()));
   dataTimer.start(100); // Interval 0 means to refresh as fast as possible
@@ -70,31 +52,35 @@ void MainWindow::realtimeDataSlot()
 {
   // calculate two new data points:
 
-  double key = QDateTime::currentDateTime().toMSecsSinceEpoch()/1000.0;
+    double key = QDateTime::currentDateTime().toMSecsSinceEpoch()/1000.0;
 
-  static double lastPointKey = 0;
+    // static double lastPointKey = 0;
 
-  if (key-lastPointKey > 0.1) // at most add point every 10 ms
-  {
+  // if (key-lastPointKey > 0.01) // at most add point every 10 ms
+  //  {
 
-    double value0 = qCos(key);  //qSin(key*1.6+qCos(key*1.7)*2)*10 + qSin(key*1.2+0.56)*20 + 26;
+    QVector<double> x(101), y(101); // initialize with entries 0..100
+
+    for (int i=0; i<101; ++i)
+    {
+      x[i] = i; // x goes from -1 to 1
+      y[i] = rand() % 20+1;  // let's plot a quadratic function
+    }
 
     // add data to lines:
-    ui->customPlot->graph(0)->addData(key, value0);
+    ui->customPlot->graph(0)->addData(x,y);
 
     // set data of dots:
 
     // remove data of lines that's outside visible range:
-    ui->customPlot->graph(0)->removeDataBefore(key-8);
+    // ui->customPlot->graph(0)->removeDataBefore(key-8);
 
     // rescale value (vertical) axis to fit the current data:
     ui->customPlot->graph(0)->rescaleValueAxis();
 
-    lastPointKey = key;
-  }
-  // make key axis range scroll with the data (at a constant range size of 8):
-  ui->customPlot->xAxis->setRange(key+0.25, 8, Qt::AlignRight);
-  ui->customPlot->replot();
-
+    // }
+    // make key axis range scroll with the data (at a constant range size of 8):
+    ui->customPlot->xAxis->setRange(1,20);
+    ui->customPlot->replot();
 
 }
